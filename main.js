532950a985b8ee68c965cb994a4917bbd15f0ff9
Matthew <matthew@Front-End-iMac.local>
2014-05-14 13:19:02 +0100

    main.js file added

---
+++ main.js
@@ -0,0 +1,37 @@
+/*
+ * created by info@webegg.co.uk
+ */
+
+$(function($) {
+    // Cache a reference to $(window), for performance, and get the initial dimensions of the window
+    var $window = $(window),
+        previousDimensions = {
+            width: $window.width(),
+            height: $window.height()
+        };
+
+    $window.resize( $.debounce( 250, false, function(e){
+    	
+        var newDimensions = {
+            width: $window.width(),
+            height: $window.height()
+        };
+
+        if (newDimensions.width > previousDimensions.width) {
+            // scaling up
+            if((newDimensions.width > 480) && (previousDimensions.width < 480)){
+		        location.reload();
+		        console.log('scaling up');
+		    }
+        } else {
+            // scaling down
+            if((newDimensions.width < 480) && (previousDimensions.width > 480)){
+		        location.reload();
+		        console.log('scaling down');
+		    }
+        }
+
+        // Store the new dimensions
+        previousDimensions = newDimensions;
+    }));
+});
